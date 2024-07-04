d7c21270235e286dcf4851add9e8e8f56d2e4c70
Onah Victor <victoronah.dev@gmail.com>
2024-07-04 01:43:14 +0100

    Update: Removed BrowserRouter in favor of HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 import Root from "./pages/Root";
@@ -11,38 +11,38 @@ import Cart from "./pages/Cart";
 import Checkout from "./pages/Checkout";
 import Search from "./pages/Search";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    children: [
-      {
-        index: true,
-        element: <Home />,
-      },
-      {
-        path: "/products/:id",
-        element: <Product />,
-        loader: productLoader,
-      },
-      {
-        path: "/cart",
-        element: <Cart />,
-      },
-      {
-        path: "/checkout",
-        element: <Checkout />,
-      },
-      {
-        path: "/search",
-        element: <Search />,
-      },
-    ],
-  },
+const router = createHashRouter([
+	{
+		path: "/",
+		element: <Root />,
+		children: [
+			{
+				index: true,
+				element: <Home />
+			},
+			{
+				path: "/products/:id",
+				element: <Product />,
+				loader: productLoader
+			},
+			{
+				path: "/cart",
+				element: <Cart />
+			},
+			{
+				path: "/checkout",
+				element: <Checkout />
+			},
+			{
+				path: "/search",
+				element: <Search />
+			}
+		]
+	}
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <Provider store={store}>{<RouterProvider router={router} />}</Provider>
-  </React.StrictMode>
+	<React.StrictMode>
+		<Provider store={store}>{<RouterProvider router={router} />}</Provider>
+	</React.StrictMode>
 );
