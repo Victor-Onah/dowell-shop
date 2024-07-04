37fc62b37cca2e5d6ad5f848e46c4a7d700c1fd6
Onah Victor <victoronah.dev@gmail.com>
2024-07-04 02:00:29 +0100

    Update: Added scroll to top functinality when transitioning between pages

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,8 +1,13 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import { useLayoutEffect } from "react";
 
 const Root = () => {
+  const location = useLocation();
+
+  useLayoutEffect(() => window.scrollTo({ top: 0 }), [location]);
+
   return (
     <>
       <Header />
